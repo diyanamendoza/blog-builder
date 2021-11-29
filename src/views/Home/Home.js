981131e0864f169e249ae17a02b0a86ee0e9153c
diyanamendoza <diyana.amp@pm.me>
2021-11-29 12:44:40 -0800

    Add state and props to Home.js

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useState } from 'react'
 import Preview from '../../components/Preview/Preview'
 import Editor from '../../components/Editor/Editor'
 
@@ -7,12 +7,28 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('')
+  const [font, setFont] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [align, setAlign] = useState('')
+  const [text, setText] = useState('')
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} subtitle={subtitle} font={font} text={text} align={align} />
+      <Editor
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        align={align}
+        setAlign={setAlign}
+        font={font}
+        setFont={setFont}
+        text={text}
+        setText={setText}
+      />
     </main>
   )
 }
